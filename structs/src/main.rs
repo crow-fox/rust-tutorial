ef375623c5d8fd8b59ca32743d58aa8f647097a2
crow-fox <surakamiya198@gmail.com>
2024-05-03 20:07:26 +0900

    structs

---
+++ structs/src/main.rs
@@ -0,0 +1,47 @@
+fn main() {
+    let user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    let mut user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    user1.email = String::from("anotheremail@example.com");
+
+    let user2 = User {
+        email: String::from("another@example.com"),
+        username: String::from("anotherusername456"),
+        ..user1 // JSのように上書きされず、残りのフィールドだけ指定される
+    };
+
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+    // blackとoriginは異なる型
+    // TSのように構造体内のフィールドが同じ型であっても異なる型として扱われる
+}
+
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 1,
+    }
+}
+
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
